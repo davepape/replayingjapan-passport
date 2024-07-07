24f49dacdcdd7f0b4b7b5424d46c19d9d74c9e55
Dave Pape <depape@buffalo.edu>
2024-07-07 12:52:13 -0700

    add basic registration form

--- views.py
+++ views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 
 def index(request):
-    x = request.session.get('name','unknown')
-    request.session['name'] = "Dave"
-    return HttpResponse("Hello, " + x + ". You're at the test index.")
+    context = {}
+    return render(request, 'register.html', context)
+
+def newperson(request):
+    name = request.POST.get('yourname','')
+    email = request.POST.get('youremail','')
+    request.session['name'] = name
+    context = { 'name': name }
+    return render(request, 'welcome.html', context)
+
